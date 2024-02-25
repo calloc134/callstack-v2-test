b3d3bb255c1e37090593457281a12fe3c46798de
calloc134 <115209243+calloc134@users.noreply.github.com>
2024-02-25 12:02:11 +0900

    chore: add comment to reference

--- build.ts
+++ build.ts
@@ -10,7 +10,7 @@ await build({
     ".js": ".mjs",
   },
   banner: {
-    // commonjs用ライブラリをESMプロジェクトでbundleする際に生じることのある問題への対策
+    // refer to https://zenn.dev/junkor/articles/2bcd22ca08d21d
     js: 'import { createRequire } from "module"; import url from "url"; const require = createRequire(import.meta.url); const __filename = url.fileURLToPath(import.meta.url); const __dirname = url.fileURLToPath(new URL(".", import.meta.url));',
   },
 });
