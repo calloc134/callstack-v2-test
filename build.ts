3092eb61eb66e7d234ca2028b3c8ab8feb4e4c5c
calloc134 <115209243+calloc134@users.noreply.github.com>
2024-02-25 11:57:45 +0900

    refactor: change extention (*.mjs)

--- build.ts
+++ build.ts
@@ -6,6 +6,9 @@ await build({
   platform: "node",
   outbase: "src",
   outdir: "dist",
+  outExtension: {
+    ".js": ".mjs",
+  },
   banner: {
     // commonjs用ライブラリをESMプロジェクトでbundleする際に生じることのある問題への対策
     js: 'import { createRequire } from "module"; import url from "url"; const require = createRequire(import.meta.url); const __filename = url.fileURLToPath(import.meta.url); const __dirname = url.fileURLToPath(new URL(".", import.meta.url));',
