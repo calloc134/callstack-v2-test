c0c91ba065e25bd0c76e9f7c15e30d0f260d155e
calloc134 <115209243+calloc134@users.noreply.github.com>
2024-02-25 13:21:39 +0900

    build: add settings tsConfigPaths

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import react from "@vitejs/plugin-react-swc";
+import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite()],
+  plugins: [react(), TanStackRouterVite(), tsConfigPaths()],
 });
