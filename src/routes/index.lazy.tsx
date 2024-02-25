1167b2858c1407ea635af3c5433b7a1ce54d8b78
calloc134 <115209243+calloc134@users.noreply.github.com>
2024-02-25 13:24:24 +0900

    chore: add shadcn button for test

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import { createLazyFileRoute } from "@tanstack/react-router";
 
 export const Route = createLazyFileRoute("/")({
@@ -8,6 +9,7 @@ function Index() {
   return (
     <div className="p-2">
       <h3>Welcome Home!</h3>
+      <Button>Click me</Button>
     </div>
   );
 }
