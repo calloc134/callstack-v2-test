6dcd56cf92a011e9091e634411d52ee39bcff9b7
calloc134 <115209243+calloc134@users.noreply.github.com>
2024-03-01 12:48:12 +0000

    chore: add env.ts

---
+++ packages/backend/src/infrastructure/env/env.ts
@@ -0,0 +1,4 @@
+export const db_host = process.env.POSTGRES_HOST || "localhost";
+export const db_user = process.env.POSTGRES_USER;
+export const db_password = process.env.POSTGRES_PASSWORD;
+export const db_database = process.env.POSTGRES_DB || "db";
