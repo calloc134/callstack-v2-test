9c8c9e0d29b58b5e99627f168f2860d8978a062b
calloc134 <115209243+calloc134@users.noreply.github.com>
2024-03-02 02:52:05 +0000

    build: setup build config for esbuild

--- packages/backend/build.ts
+++ packages/backend/build.ts
@@ -6,9 +6,12 @@ await build({
   platform: "node",
   outbase: "src",
   outdir: "dist",
+  packages: "external",
   outExtension: {
     ".js": ".mjs",
   },
+  bundle: true,
+  sourcemap: true,
   banner: {
     // refer to https://zenn.dev/junkor/articles/2bcd22ca08d21d
     js: 'import { createRequire } from "module"; import url from "url"; const require = createRequire(import.meta.url); const __filename = url.fileURLToPath(import.meta.url); const __dirname = url.fileURLToPath(new URL(".", import.meta.url));',
