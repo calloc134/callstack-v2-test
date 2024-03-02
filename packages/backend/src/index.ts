4887813b8165581dcf1f42f521349fe544c59f9f
calloc134 <115209243+calloc134@users.noreply.github.com>
2024-03-02 02:56:47 +0000

    chore: index.ts

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,9 +1,18 @@
 import { createServer } from "node:http";
 import { createYoga } from "graphql-yoga";
+import { schema } from "./infrastructure/yoga/schema";
+import { createDBPool } from "./infrastructure/db/connect";
+import { drizzle } from "drizzle-orm/node-postgres";
+
+const db_pool = createDBPool();
 
 const yoga = createYoga({
   graphqlEndpoint: "/api/graphql",
   graphiql: true,
+  schema,
+  context: {
+    db: drizzle(db_pool),
+  },
 });
 
 const server = createServer(yoga);
@@ -12,8 +21,11 @@ server.listen(6173, () => {
   console.log("🚀 Server is running");
 });
 
-process.on("SIGINT", () => {
+process.on("SIGINT", async () => {
+  console.log("🛑 Stopping server");
+  await db_pool.end();
   server.close(() => {
     console.log("🛑 Server stopped");
+    process.exit(0);
   });
 });
