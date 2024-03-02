ee246be758dbd31e3b24fa43e6d627e7d65f9811
calloc134 <115209243+calloc134@users.noreply.github.com>
2024-03-02 02:53:46 +0000

    chore: setup resolver config

--- packages/backend/src/infrastructure/env/env.ts
+++ packages/backend/src/infrastructure/env/env.ts
@@ -2,3 +2,4 @@ export const db_host = process.env.POSTGRES_HOST || "localhost";
 export const db_user = process.env.POSTGRES_USER;
 export const db_password = process.env.POSTGRES_PASSWORD;
 export const db_database = process.env.POSTGRES_DB || "db";
+export const schema_path = process.env.SCHEMA_PATH || "";
