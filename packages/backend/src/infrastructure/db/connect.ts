d96bbccf148574ff42e8d5ac9350395e4d24db2d
calloc134 <115209243+calloc134@users.noreply.github.com>
2024-03-02 02:56:28 +0000

    refactor: db pool

--- packages/backend/src/infrastructure/db/connect.ts
+++ packages/backend/src/infrastructure/db/connect.ts
@@ -1,14 +1,11 @@
-import { drizzle } from "drizzle-orm/node-postgres";
-import { Pool } from "pg";
+import pg from "pg";
 import { db_database, db_host, db_password, db_user } from "../env/env";
 
-export const createDBClient = () => {
-  const pool = new Pool({
+export const createDBPool = () => {
+  return new pg.Pool({
     host: db_host,
     user: db_user,
     password: db_password,
     database: db_database,
   });
-
-  return drizzle(pool);
 };
